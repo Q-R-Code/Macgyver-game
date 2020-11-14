62f9a11bcbc482262a274c9626a5b27665d6a9ab
Quentin R <qruffier@gmail.com>
2020-11-14 12:04:09 +0100

    ajout du deplacement de macgyver, dee nouvelles images, changement du background

--- main.py
+++ main.py
@@ -23,11 +23,17 @@ while running:
     screen.blit(guardian.image, guardian.rect)
     maze = Maze()
     maze.generate()
-    maze.print(screen)
+    maze.print_maze(screen)
     pygame.display.flip()
 
-    if pressed.get(pygame.K_RIGHT) and macgyver.rect.x != maze.print(wall):
+    if pressed.get(pygame.K_RIGHT) and macgyver.rect.x + 60 < 900:
         macgyver.move_right()
+    elif pressed.get(pygame.K_LEFT) and macgyver.rect.x > 0:
+        macgyver.move_left()
+    elif pressed.get(pygame.K_UP) and macgyver.rect.y > 0:
+        macgyver.move_up()
+    elif pressed.get(pygame.K_DOWN) and macgyver.rect.y + 60 < 900:
+        macgyver.move_down()
 
 
     pygame.display.flip()
@@ -37,3 +43,8 @@ while running:
             running = False
             pygame.quit()
             print("Fermeture du jeu.")
+
+        elif event.type == pygame.KEYDOWN:
+            pressed[event.key] = True
+        elif event.type == pygame.KEYUP:
+            pressed[event.key] = False
--- maze_map.py
+++ maze_map.py
@@ -1,5 +1,4 @@
 import pygame
-from pygame.locals import *
 from constantes import *
 
 
@@ -7,6 +6,9 @@ class Maze(pygame.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
+
+        self.wall = pygame.image.load("images/wall.png")
+        self.rect = self.wall.get_rect()
 
     def generate(self):
         maze_files = open("maze.txt", "r")
@@ -17,26 +19,21 @@ class Maze(pygame.sprite.Sprite):
                 if letters != "\n":
                     maze_ligne.append(letters)
             maze_structure.append(maze_ligne)
-        #print(maze_structure)
         self.structure = maze_structure
 
-    def print(self, screen):
+    def print_maze(self, screen):
         numb_ligne = 0
-        numb_O = 0
-        numb_W = 0
         spawn = 0
         end = 0
-        wall = pygame.image.load("images/wall.png")
         for ligne in self.structure:
             numb_colonne = 0
             for letters in ligne:
                 x = numb_colonne * taille_sprite
                 y = numb_ligne * taille_sprite
                 if letters == "O":
-                    numb_O += 1
+                    pass
                 elif letters == "W":
-                    screen.blit(wall, (x, y))
-                    numb_W += 1
+                    screen.blit(self.wall, (x, y))
                 elif letters == "A":
                     spawn += 1
                 elif letters == "B":
@@ -45,5 +42,6 @@ class Maze(pygame.sprite.Sprite):
                     pass
                 numb_colonne += 1
             numb_ligne += 1
-        # print(numb_ligne, numb_colonne)
-        # print(numb_W, numb_O)
+
+    def check_collision(self, macgyver):
+        return pygame.sprite.spritecollide(macgyver,self.wall, False, pygame.sprite.collide_mask)
